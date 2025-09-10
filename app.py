d18d98b3e49b28ab8613aeab13865e6c333e4e03
Saswat Samal <saswat1091samal@gmail.com>
2025-09-10 21:03:13 +0530

    add path names

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 import pickle
 import streamlit as st
 import requests
+import os
+
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+movies_path = os.path.join(BASE_DIR, 'model', 'movie_list.pkl')
+similarity_path = os.path.join(BASE_DIR, 'model', 'similarity.pkl')
 
 def fetch_poster(movie_id):
     url = "https://api.themoviedb.org/3/movie/{}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US".format(movie_id)
@@ -25,8 +32,8 @@ def recommend(movie):
 
 
 st.header('Movie Recommender System')
-movies = pickle.load(open('model/movie_list.pkl','rb'))
-similarity = pickle.load(open('model/similarity.pkl','rb'))
+movies = pickle.load(open(movies_path,'rb'))
+similarity = pickle.load(open(similarity_path,'rb'))
 
 movie_list = movies['title'].values
 selected_movie = st.selectbox(
